1e06ddcff5504fb15db667e4026c3e88a2b1043f
mvitali01 <105487401+mvitali01@users.noreply.github.com>
2025-02-06 17:31:25 +0100

    Indirizzo IP macchina EC2 nascosto

--- src/pages/DataTable.js
+++ src/pages/DataTable.js
@@ -49,7 +49,7 @@ const DataTable=()=>{
                 localStorage.setItem("contenuto",xhr.responseText)
             }
         };
-        xhr.open('GET','http://34.225.255.18:3000/api/v1/file/generated');
+        xhr.open('GET','http://<IP_EC2>:3000/api/v1/file/generated');
         xhr.send()
         check_consistance()
     }
